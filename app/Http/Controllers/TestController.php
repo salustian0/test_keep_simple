7558bb1d8a39bf11189fbe2bd7851623a4026829
Renan Salustiano <renan.silva621@etec.sp.gov.br>
2023-08-11 03:54:35 -0300

    Criação da action para o teste 3 + action para a exportação csv

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\SalustianoCsvService;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
 
+    public function __construct(
+        private SalustianoCsvService $salustianoCsvService
+    ){}
 
     public function test1(){
         return view('test1');
@@ -14,5 +18,18 @@ class TestController extends Controller
 
     public function test2(){
         return view('test2');
+    }
+
+    public function test3(){
+        return view('test3');
+    }
+
+    public function exportCsv(Request $request) {
+        try{
+            $dataToExport = $request->input('dataToExport');
+            $this->salustianoCsvService->exportCsv($dataToExport);
+        }catch (\Exception $ex){
+            return response()->json(['error' => 'Parâmetros informados incorretamente.'], 400);
+        }
     }
 }
